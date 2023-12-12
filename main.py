7ba7ab2463f990eb17fa4f0303d625048482987f
UserName504 <dszalai@hotmail.co.uk>
2023-12-12 16:05:37 +0000

    Initial commit.

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ async def adjust_volume_api(request: Request):
         target_dBFS = data.get('target_dBFS')
         if not input_file or not output_file or target_dBFS is None:
             raise HTTPException(status_code=400, detail='Invalid input')
-        # Check if the file has a valid audio extension
         valid_audio_formats = ["wav", "mp3", "ogg", "flac", "aac", "m4a"]
         file_extension = input_file.split('.')[-1].lower()
         if file_extension not in valid_audio_formats:
@@ -29,14 +28,10 @@ async def adjust_volume_api(request: Request):
     except Exception as e:
         raise HTTPException(status_code=500, detail='Incorrect file type.' + str(e))
 
-@app.get("/test/")
-async def the_test():
-    return {"key": "value", "message": "hello world" }
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
     try:
-        # Save the uploaded file in the "uploads" directory
         uploads_dir = Path("uploads")
         uploads_dir.mkdir(parents=True, exist_ok=True)
         file_path = uploads_dir / file.filename
@@ -52,7 +47,6 @@ async def create_upload_file(file: UploadFile = File(...)):
 @app.get("/downloadfile/{filename}")
 async def read_upload_file(filename: str):
     try:
-        # Return the requested file for download
         file_path = Path("uploads") / filename
         if file_path.exists():
             return FileResponse(file_path, filename=filename)
@@ -61,20 +55,20 @@ async def read_upload_file(filename: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @app.get("/listfiles/")
 async def list_files():
     try:
-        # List all files in the "uploads" directory
         uploads_dir = Path("uploads")
         file_list = [str(file.name) for file in uploads_dir.iterdir() if file.is_file()]
         return {"files": file_list}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @app.delete("/deletefile/{filename}")
 async def delete_file(filename: str):
     try:
-        # Delete the specified file from the "uploads" directory
         file_path = Path("uploads") / filename
         if file_path.exists():
             file_path.unlink()
@@ -83,27 +77,8 @@ async def delete_file(filename: str):
             raise HTTPException(status_code=404, detail="File not found")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-#
-# Function to get the OpenAPI schema
-def get_openapi_schema():
-    return get_openapi(
-        title="Audio Restful APIYour API Title",
-        version="3.1.0",
-        description="An API to upload, download, and alter the dBFS of audio files.",
-        routes=app.routes,
-    )
 
-# Save the OpenAPI schema to a file
-def save_openapi_schema():
-    openapi_schema = get_openapi_schema()
-    file_path = Path("openapi.json")
-    with file_path.open("w") as file:
-        json.dump(openapi_schema, file, indent=2)
-#
+
 if __name__ == '__main__':
     import uvicorn
-
-    # Save the OpenAPI schema before running the application
-    save_openapi_schema()
-
     uvicorn.run(app, host='127.0.0.1', port=8000)
--- test_main.py
+++ test_main.py
@@ -5,94 +5,62 @@ from main import app
 client = TestClient(app)
 
 def test_upload_file():
-    # Prepare a test file
     test_file_content = b"test file content"
-    test_file_name = "test_upload_file.txt"     #Just a blank .txt file, for the sake of demonstration.
-    # Make a request to the upload file endpoint
-    response = client.post("/uploadfile/", files={"file": (test_file_name, test_file_content)})
-    # Assert the response status code is 200 OK
-    assert response.status_code == 200
-    # Assert the response JSON contains the uploaded file name
-    assert response.json() == {"filename": test_file_name}
-    # Clean up: Delete the test file
-    client.delete(f"/deletefile/{test_file_name}")
-    # Ensure the test file is deleted
-    assert not (Path("uploads") / test_file_name).exists()
+    test_file_name = "test_upload_file.txt"                 #Just a blank .txt file, for the sake of demonstration.
+    response = client.post("/uploadfile/", files={"file": (test_file_name, test_file_content)}) #Make an endpoint request.
+    assert response.status_code == 200                      #Make sure that the status code is 200.
+    assert response.json() == {"filename": test_file_name}  #Make sure that the JSON data contains the uploaded file name.
+    client.delete(f"/deletefile/{test_file_name}")          #Delete the test file.
+    assert not (Path("uploads") / test_file_name).exists()  #Make sure that hte test file has been deleted.
 
 def test_list_files():
-    # Prepare a test file
     test_file_content = b"test file content"
-    test_file_name = "test_list_files.txt"      #Just a blank .txt file, for the sake of demonstration.
-    # Make a request to the upload file endpoint
+    test_file_name = "test_list_files.txt"
     response_upload = client.post("/uploadfile/", files={"file": (test_file_name, test_file_content)})
-    # Assert the upload response status code is 200 OK
     assert response_upload.status_code == 200
-    # Make a request to the list files endpoint
     response_list = client.get("/listfiles/")
-    # Assert the list response status code is 200 OK
     assert response_list.status_code == 200
-    # Assert the response JSON contains the uploaded file name
     assert test_file_name in response_list.json()["files"]
-    client.delete(f"/deletefile/{test_file_name}")          #Delete the test file.
-    assert not (Path("uploads") / test_file_name).exists()  #Make sure that hte test file has been deleted.
+    client.delete(f"/deletefile/{test_file_name}")
+    assert not (Path("uploads") / test_file_name).exists()
 
 def test_download_file():
-    # Prepare a test file
     test_file_content = b"test file content"
-    test_file_name = "test_download_file.txt"   #Just a blank .txt file, for the sake of demonstration.
-    # Make a request to the upload file endpoint
+    test_file_name = "test_download_file.txt"
     response_upload = client.post("/uploadfile/", files={"file": (test_file_name, test_file_content)})
-    # Assert the upload response status code is 200 OK
     assert response_upload.status_code == 200
-    # Make a request to the download file endpoint
     response_download = client.get(f"/downloadfile/{test_file_name}")
-    # Assert the download response status code is 200 OK
     assert response_download.status_code == 200
-    # Assert the response content matches the uploaded file content
     assert response_download.content == test_file_content
-    client.delete(f"/deletefile/{test_file_name}")          #Delete the test file.
-    assert not (Path("uploads") / test_file_name).exists()  #Make sure that hte test file has been deleted.
+    client.delete(f"/deletefile/{test_file_name}")
+    assert not (Path("uploads") / test_file_name).exists()
 
 def test_delete_file():
-    # Prepare a test file
     test_file_content = b"test file content"
     test_file_name = "test_delete_file.txt"
-    # Make a request to the upload file endpoint
     response_upload = client.post("/uploadfile/", files={"file": (test_file_name, test_file_content)})
-    # Assert the upload response status code is 200 OK
     assert response_upload.status_code == 200
-    # Make a request to the delete file endpoint
     response_delete = client.delete(f"/deletefile/{test_file_name}")
-    # Assert the delete response status code is either 200 OK or 404 Not Found
     assert response_delete.status_code in [200, 404]
     if response_delete.status_code == 200:
-        # Ensure the test file is deleted
         assert not (Path("uploads") / test_file_name).exists()
     elif response_delete.status_code == 404:
-        # Assert the response JSON contains a message about the file not found
         assert response_delete.json()["detail"] == "File not found"
 
 def test_adjust_volume():
-    # Prepare a test audio file
     test_audio_content = b"test audio content"
     test_audio_name = "test_adjust_volume.wav"  #The same as AudioExample01.wav, but with a different name.
-    # Make a request to the upload file endpoint
     response_upload = client.post("/uploadfile/", files={"file": (test_audio_name, test_audio_content)})
-    # Assert the upload response status code is 200 OK
     assert response_upload.status_code == 200
-    # Make a request to the adjust volume endpoint
     data = {
         "input_file": f"uploads/{test_audio_name}",
         "output_file": f"uploads/adjusted_{test_audio_name}",
-        "target_dBFS": -10  # Adjust the volume by -10 dBFS (adjust as needed)
+        "target_dBFS": -50
     }
-
     response_adjust_volume = client.post("/adjust_volume", json=data)
-    # Assert the adjust volume response status code is either 200 OK or 500 Internal Server Error
     assert response_adjust_volume.status_code in [200, 500]
     if response_adjust_volume.status_code == 200:
-        # Assert the adjusted file exists
         adjusted_file_path = Path("uploads/adjusted_") / test_audio_name
         assert adjusted_file_path.exists()
-    client.delete(f"/deletefile/{test_audio_name}")             #Delete the test file.
-    client.delete(f"/deletefile/adjusted_{test_audio_name}")    #Make sure that hte test file has been deleted.
+    client.delete(f"/deletefile/{test_audio_name}")
+    client.delete(f"/deletefile/adjusted_{test_audio_name}")
